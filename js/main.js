177f3163215b12dfc0924b439200140b5e14c0d7
Axel Torrico <torricohernan2@gmail.com>
2022-09-22 10:57:40 -0300

    agregue nuevos estilos y horas

--- js/main.js
+++ js/main.js
@@ -13,6 +13,7 @@ let stopwatchInterval;
 let runningTime = 0;
 
 const playPause = () => {
+  // si ese boton tiene la clase running esta andando
   const isPaused = !playPauseButton.classList.contains('running');
 
   if (isPaused) {
@@ -32,7 +33,6 @@ const pause = () => {
 const stop = () => {
   secondsSphere.style.transform = 'rotate(-90deg) translate(93px)';
   secondsSphere.style.animation = 'none';
-  playPauseButton.classList.remove('running');
   runningTime = 0;
   clearInterval(stopwatchInterval);
   stopwatch.textContent = '00:00';
@@ -41,7 +41,6 @@ const stop = () => {
 const start = () => {
   secondsSphere.style.animation = 'rotacion 60s linear infinite';
   let startTime = Date.now() - runningTime;
-  /* secondsSphere.style.animationPlayState = 'running'; */
   stopwatchInterval = setInterval(() => {
     runningTime = Date.now() - startTime;
     stopwatch.textContent = calculateTime(runningTime);
@@ -51,11 +50,13 @@ const start = () => {
 const calculateTime = runningTime => {
   const total_seconds = Math.floor(runningTime / 1000);
   const total_minutes = Math.floor(total_seconds / 60);
+  const total_hours = Math.floor(total_minutes / 60);
 
   const display_seconds = (total_seconds % 60).toString().padStart(2, "0");
   const display_minutes = total_minutes.toString().padStart(2, "0");
+  const display_hours = total_hours.toString().padStart(2, "0");
 
-  return `${display_minutes}:${display_seconds}`
+  return `${display_hours}:${display_minutes}:${display_seconds}`
 }
 
 playPauseButton.addEventListener('click', () => {
